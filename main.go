1aeb5bfcc19a6a4fee32e2a2dbbde35f2c55e2e6
dm <dmitry@udalov.online>
2025-01-12 15:10:24 +0600

    init

--- main.go
+++ main.go
@@ -4,16 +4,42 @@ import (
 	"atomic-actions/models/installer"
 	"fmt"
 	tea "github.com/charmbracelet/bubbletea"
+	"io"
+	"log"
 	"os"
+	"path/filepath"
 )
 
 func main() {
-	f, err := tea.LogToFile("debug.log", "debug")
+	logFile := setLogger()
+	defer logFile.Close()
+
+	installer.Run()
+}
+
+func setLogger() *os.File {
+	tempDir := os.TempDir()
+	logFilePath := filepath.Join(tempDir, "debug.log")
+	logFile, err := os.OpenFile(logFilePath, os.O_WRONLY|os.O_CREATE|os.O_APPEND, 0o600)
+	if err != nil {
+		fmt.Println("fatal:", err)
+		os.Exit(1)
+	}
+
+	// Настраиваем MultiWriter для записи в файл и консоль
+	multiWriter := io.MultiWriter(os.Stdout, logFile)
+	log.SetOutput(multiWriter)
+
+	teaLogWriter := io.MultiWriter(os.Stdout, logFile)
+	teaLog := log.New(teaLogWriter, "debug ", log.LstdFlags)
+	f, err := tea.LogToFileWith(logFilePath, "debug", teaLog)
 	if err != nil {
 		fmt.Println("fatal:", err)
 		os.Exit(1)
 	}
 	defer f.Close()
 
-	installer.Run()
+	//log.Printf("Логи записываются в файл: %s\n", logFilePath)
+
+	return logFile
 }
