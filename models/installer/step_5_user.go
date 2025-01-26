98f542231567838ca154125045d33476b51b117e
dm <dmitry@udalov.online>
2025-01-26 17:48:34 +0600

    init

--- models/installer/step_5_user.go
+++ models/installer/step_5_user.go
@@ -59,7 +59,7 @@ func (m UserCreation) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 	switch msg := msg.(type) {
 	case tea.KeyMsg:
 		switch msg.String() {
-		case "ctrl+c", "q":
+		case "ctrl+c":
 			return m, tea.Quit
 		case "tab", "down":
 			m.focusedField = (m.focusedField + 1) % 5
